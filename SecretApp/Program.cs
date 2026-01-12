590029e1b8e6cc2ccc51e814b6b0f5a7afac5bde
Thor Jonsson <thorjonsson09@gmail.com>
2026-01-12 13:43:35 +0100

    Added string lists for user passwords and usernames. Removed Writeline.

--- SecretApp/Program.cs
+++ SecretApp/Program.cs
@@ -2,9 +2,14 @@
 {
     internal class Program
     {
+        static string[] userNames = { "Pelle", "Stina", "Ali" };
+        static string[] userPasswords = { "1234", "abcd", "qwerty" };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell, World!");
+
+
+
         }
     }
 }
