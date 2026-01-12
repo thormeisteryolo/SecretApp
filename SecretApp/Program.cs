76d12ed55b58c3d1040a0d0db0c4d031590eae86
Thor Jonsson <thorjonsson09@gmail.com>
2026-01-12 14:06:48 +0100

    Added some methods and started working on the menu.

--- SecretApp/Program.cs
+++ SecretApp/Program.cs
@@ -5,11 +5,32 @@
         static string[] userNames = { "Pelle", "Stina", "Ali" };
         static string[] userPasswords = { "1234", "abcd", "qwerty" };
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            string[] choices = {"Logga in", "Lägg till användare", "Ändra Lösenord", "Avsluta" };
+            bool runtask = true;
 
 
+        }
 
+        static void AddUser()
+        {
+            Console.WriteLine("Hello from AddUser()");
+        }
+
+        static void ChangePassword()
+        {
+        
+        }
+
+        static void ShowUsers()
+        { 
+        
+        }
+
+        static void EndApplication()
+        { 
+        
         }
     }
 }
