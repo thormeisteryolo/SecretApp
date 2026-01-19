691a9990c39f801d30edff6c709d026c95e04127
Thor Jonsson <thorjonsson09@gmail.com>
2026-01-19 14:34:39 +0100

    Made a menu for selecting what user you are.

--- SecretApp/Program.cs
+++ SecretApp/Program.cs
@@ -7,7 +7,7 @@
 
         static async Task Main(string[] args)
         {
-            string[] choices = {"Logga in", "Lägg till användare", "Ändra Lösenord", "Avsluta" };
+            string[] choices = { "Logga in", "Lägg till användare", "Ändra Lösenord", "Avsluta" };
             int selektedIndex = 0;
 
             bool runtask = true;
@@ -29,37 +29,37 @@
                         Console.WriteLine($"  {choices[i]}");
                     }
                 }
-                    ConsoleKey key = Console.ReadKey(true).Key;
+                ConsoleKey key = Console.ReadKey(true).Key;
 
-                    switch(key)
-                    {
-                        case ConsoleKey.UpArrow:
-                            selektedIndex = (selektedIndex == 0) ? choices.Length - 1 : selektedIndex - 1;
-                            break;
-                        case ConsoleKey.DownArrow:
-                            selektedIndex = (selektedIndex == choices.Length - 1) ? 0 : selektedIndex + 1;
-                            break;
-                        case ConsoleKey.Enter:
-                            Console.Clear();
-                            runtask = false;
-                            switch (choices[selektedIndex])
-                            {
-                                case "Logga in":
-                                    ShowUsers();
-                                    break;
-                                case "Lägg till användare":
-                                    AddUser();
-                                    break;
-                                case "Ändra lösenord":
-                                    ChangePassword();
-                                    break;
-                                case "Avsluta":
-                                    EndApplication();
-                                    break;  
-                            }
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selektedIndex = (selektedIndex == 0) ? choices.Length - 1 : selektedIndex - 1;
                         break;
-                    }
-                
+                    case ConsoleKey.DownArrow:
+                        selektedIndex = (selektedIndex == choices.Length - 1) ? 0 : selektedIndex + 1;
+                        break;
+                    case ConsoleKey.Enter:
+                        Console.Clear();
+                        runtask = false;
+                        switch (choices[selektedIndex])
+                        {
+                            case "Logga in":
+                                ShowUsers();
+                                break;
+                            case "Lägg till användare":
+                                AddUser();
+                                break;
+                            case "Ändra lösenord":
+                                ChangePassword();
+                                break;
+                            case "Avsluta":
+                                EndApplication();
+                                break;
+                        }
+                        break;
+                }
+
             }
         }
 
@@ -70,17 +70,59 @@
 
         static void ChangePassword()
         {
-        
+
         }
 
         static void ShowUsers()
-        { 
+        {
+            int namesIndex = 0;
+
+            bool runsecondtask = true;
+            while (runsecondtask == true)
+            {
+                Console.Clear();
+                Console.WriteLine("Vem är du?");
+                for (int i = 0; i < userNames.Length; i++)
+                {
+                    if (i == namesIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.WriteLine($"->{userNames[i]}");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"  {userNames[i]}");
+                    }
+                }
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        namesIndex = (namesIndex == 0) ? userNames.Length - 1 : namesIndex - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        namesIndex = (namesIndex == userNames.Length - 1) ? 0 : namesIndex + 1;
+                        break;
+                    case ConsoleKey.Enter:
+                        Console.Clear();
+                        runsecondtask = false;
+                        break;
+                }
+            }
+        }
+
+        static void EnterPassword()
+        {
+            
+        }
+            static void EndApplication()
+            {
+            Console.WriteLine("Closing application");
+            Thread.Sleep(2000);
+            }
         
-        }
-
-        static void EndApplication()
-        { 
-
-        }
     }
 }
